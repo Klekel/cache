ca3055176aeafa2a864560e6d55db3de20190120
klekel <abagrov27@mail.ru>
2025-12-21 13:27:43 +0300

    feat: added filelist.f

---
+++ filelist.f
@@ -0,0 +1,16 @@
+src/cache_pkg.sv
+src/cache_struct_pkg.sv
+
+src/load_sm.sv
+src/lru.sv
+src/fifo.sv
+
+src/cache.sv
+src/write_back_buffer.sv
+src/main_fsm.sv
+src/axi_4_lite_master.sv
+src/axi_4_lite_slave.sv
+src/rst_sync.sv
+
+src/top.sv
+src/bram.sv
